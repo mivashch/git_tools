750fd81c4919917a1be0739aae800096e7978a5f
mivashch <matvey.ivashchenko@gmail.com>
2025-03-07 14:24:49 +0100

    migration_tool

--- gitMigrater.bash
+++ gitMigrater.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GITLAB_URL="https://gitlab-stud.elka.pw.edu.pl/mivashch"
-GITHUB_URL="https://github.com/mivashch"
+GITLAB_URL="GITLAB_URL"
+GITHUB_URL="GITHUB_URL"
 
 while read repo; do
     repo_name=$(basename "$repo" .git)
@@ -10,7 +10,7 @@ while read repo; do
     git clone --mirror "$repo"
 
     echo "Creating GitHub repo..."
-    curl -u "your_github_username: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" https://api.github.com/user/repos -d '{"name":"'"$repo_name"'"}'
+    curl -u "your_github_username:your_github_token" https://api.github.com/user/repos -d '{"name":"'"$repo_name"'"}'
 
     echo "transfering $repo_name on GitHub..."
     cd "$repo_name.git"
